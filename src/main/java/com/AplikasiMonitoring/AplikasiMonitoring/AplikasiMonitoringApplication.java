0553e2d7584f057f454fe6ad50a567f4567c2d27
Audyari <audy123ari@gmail.com>
2023-09-27 14:31:04 +0700

    Awal Install Spring Boot

---
+++ src/main/java/com/AplikasiMonitoring/AplikasiMonitoring/AplikasiMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.AplikasiMonitoring.AplikasiMonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AplikasiMonitoring/AplikasiMonitoring/AplikasiMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AplikasiMonitoring.AplikasiMonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
